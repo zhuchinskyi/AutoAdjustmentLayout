641033930898b1fee80ef5705398c1f87c333446
Your Name <you@example.com>
2015-11-10 11:58:19 +0200

    Added lib samples

---
+++ app/src/main/java/com/example/autoadjustmentlayout/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.autoadjustmentlayout;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.compoundview.autoadjustmentlayout.AutoAdjustmentLayout;
+import com.example.autoadjustmentlayout.R;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        AutoAdjustmentLayout autoAdjustmentView = (AutoAdjustmentLayout) findViewById(R.id.autoAdjustmentView);
+        autoAdjustmentView.setElementList(getRandomItems(20));
+        autoAdjustmentView.isRemovable(true);
+        autoAdjustmentView.setAutoAdjustmentLayoutListener(new AutoAdjustmentLayout.IOnAutoAdjustmentLayoutListener() {
+            @Override
+            public void onLastItemRemoved(View view) {
+                Snackbar.make(view, "That's all !!!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+
+            @Override
+            public void onItemRemoved(View view) {
+                Snackbar.make(view, "Removed view: " + ((TextView) view).getText(), Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
+
+        autoAdjustmentView.addElement(getTextView(156, new Random(), 2));
+    }
+
+
+    @NonNull
+    private ArrayList<Object> getRandomItems(int count) {
+        int prevTextViewId = 0;
+        Random rnd = new Random();
+        ArrayList<Object> items = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            final TextView textView = getTextView(prevTextViewId, rnd, i);
+
+            items.add(textView);
+        }
+        return items;
+    }
+
+    @NonNull
+    private TextView getTextView(int prevTextViewId, Random rnd, int i) {
+        final TextView textView = new TextView(this);
+        textView.setPadding(5, 5, 5, 5);
+        textView.setText("Text " + i);
+        textView.setTextColor(rnd.nextInt() | 0xff000000);
+        int curTextViewId = prevTextViewId + 1;
+        textView.setId(curTextViewId);
+        return textView;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ library/src/main/java/com/compoundview/autoadjustmentlayout/AutoAdjustmentLayout.java
@@ -0,0 +1,238 @@
+package com.compoundview.autoadjustmentlayout;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Compound view to build views with auto adjustment according to views parent layout width
+ * <p/>
+ * Created by d.zhuchinskiy on 11/6/15.
+ */
+public class AutoAdjustmentLayout extends LinearLayout {
+
+    private String TAG = "AutoAdjustmentLayout";
+
+    int mWidth;
+    int mHeight;
+
+    LinearLayout mTmpLayout;
+
+    Context mContext;
+
+    ArrayList<Object> mElementList;
+
+    boolean mIsRemovable = false;
+
+    private IOnAutoAdjustmentLayoutListener mAutoAdjustmentLayoutListener;
+
+
+    public AutoAdjustmentLayout(Context context) {
+        super(context);
+        initializeViews(context);
+    }
+
+    public AutoAdjustmentLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initializeViews(context);
+    }
+
+
+    /**
+     * Set orientation, etc.
+     *
+     * @param context the current mContext for the view.
+     *
+     * @ Calculating height & width of parent layout, then building view.
+     * @ Removing on globalLayoutListener after
+     */
+    private void initializeViews(Context context) {
+        setOrientation(VERTICAL);
+        this.mContext = context;
+
+        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @SuppressLint("NewApi")
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onGlobalLayout() {
+                Log.i(TAG, "onGlobalLayout");
+
+                updateSizeInfo();
+
+                if (mElementList != null)
+                    buildView(mElementList);
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN)
+                    getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                else
+                    getViewTreeObserver().removeGlobalOnLayoutListener(this);
+            }
+        });
+
+    }
+
+    /**
+     * @ Recalculate width and height of view.
+     */
+    private void updateSizeInfo() {
+        mWidth = getWidth();
+        mHeight = getHeight();
+        Log.v(TAG, mWidth + "-" + mHeight);
+    }
+
+    /**
+     * @ Building current view according to input and layout params.
+     */
+    private void buildView(final ArrayList<Object> items) {
+        removeAllViews();
+
+        int currentTextViewSizeInLine = 0;
+
+        mTmpLayout = getLinearLayout();
+
+        addView(mTmpLayout);
+
+        for (int i = 0; i < items.size(); i++) {
+            TextView textView = (TextView) items.get(i);
+
+            if (mIsRemovable) {
+                textView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ArrayList newArrayList = (ArrayList) items.clone();
+
+                        newArrayList.remove(v);
+
+                        if (newArrayList.size() > 0) {
+                            mAutoAdjustmentLayoutListener.onItemRemoved(v);
+                        } else {
+                            mAutoAdjustmentLayoutListener.onLastItemRemoved(v);
+                        }
+
+                        buildView(newArrayList);
+
+                    }
+                });
+            }
+
+            textView.measure(0, 0);       //must call measure!
+            int currTvWidth = textView.getMeasuredWidth();  //get mHeight
+            int tvHeight = textView.getMeasuredHeight(); //get mWidth
+
+            LinearLayout ll = (LinearLayout) textView.getParent();
+            if (ll != null)
+                ll.removeView(textView);
+
+            if ((currentTextViewSizeInLine + currTvWidth) >= mWidth) {
+                Log.i(TAG, "NEW LINE i: " + i);
+                mTmpLayout = getLinearLayout();
+                addView(mTmpLayout);
+                currentTextViewSizeInLine = 0;
+            }
+
+            mTmpLayout.addView(textView);
+            currentTextViewSizeInLine += currTvWidth;
+
+            Log.i(TAG, "currentTextViewSizeInLine: " + currentTextViewSizeInLine + ", mWidth: " + mWidth);
+        }
+    }
+
+
+    /**
+     * @param elementList ArrayList of Object type to render with auto adjustment. Throws NullPointerException if null passed.
+     */
+    public void setElementList(ArrayList<Object> elementList) throws NullPointerException {
+        if (elementList == null)
+            throw new NullPointerException("You can't set null");
+        this.mElementList = elementList;
+    }
+
+    /**
+     * @param element Singe Object that will be added to the end.
+     */
+    public void addElement(Object element) {
+        if (element != null) {
+            mElementList.add(element);
+            buildView(mElementList);
+        }
+    }
+
+    /**
+     * @param element Singe Object that will be added to the end.
+     * @return true if item has been removed, false otherwise
+     * @throws NullPointerException if you pass null object
+     */
+    public boolean removeElement(Object element) throws NullPointerException {
+        if (element != null) {
+            if (mElementList.remove(element)) {
+                buildView(mElementList);
+                return true;
+            }
+        } else {
+            throw new NullPointerException("Null object not allowed");
+        }
+        return false;
+    }
+
+    /**
+     * @param elementList ArrayList of Object type that will be added to the end.
+     */
+    public void addElementList(ArrayList<Object> elementList) {
+        if (mElementList == null) {
+            mElementList = new ArrayList<>();
+
+        }
+
+        if (elementList != null) {
+            mElementList.addAll(elementList);
+            buildView(mElementList);
+        }
+    }
+
+    /**
+     * @return ArrayList of Object that currently displays, null otherwise.
+     */
+    public ArrayList<Object> getmElementList() {
+        return mElementList;
+    }
+
+    /**
+     * @param isRemovable detect if elements are removable onClick. Default false.
+     */
+    public void isRemovable(boolean isRemovable) {
+        this.mIsRemovable = isRemovable;
+    }
+
+
+    public interface IOnAutoAdjustmentLayoutListener {
+        /**
+         * @ @param view which was removed
+         */
+        void onItemRemoved(View view);
+
+        /**
+         * @param view last view which was removed
+         */
+        void onLastItemRemoved(View view);
+    }
+
+    public void setAutoAdjustmentLayoutListener(IOnAutoAdjustmentLayoutListener autoAdjustmentLayoutListener) {
+        this.mAutoAdjustmentLayoutListener = autoAdjustmentLayoutListener;
+    }
+
+    private LinearLayout getLinearLayout() {
+        LinearLayout tmpLayout = new LinearLayout(mContext);
+        tmpLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        tmpLayout.setOrientation(LinearLayout.HORIZONTAL);
+        tmpLayout.setId(new Random().nextInt());
+        return tmpLayout;
+    }
+}
